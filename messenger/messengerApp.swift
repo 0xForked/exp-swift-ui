20c0eec9500018b6f2ada2ca2ef7bf1dd27738bd
A. A. Sumitro <hello@aasumitro.id>
2022-07-28 21:31:50 +0800

    Initial Commit

---
+++ messenger/messengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  messengerApp.swift
+//  messenger
+//
+//  Created by aasumitro on 28/07/22.
+//
+
+import SwiftUI
+
+@main
+struct messengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
